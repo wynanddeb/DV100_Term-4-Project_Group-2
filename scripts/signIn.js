2a29e17cdbcf3bbde79a76d82f840243e14de1bb
AlexiaCahi <231245@virtualwindow.co.za>
2023-11-06 11:14:17 +0200

    final commit

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -1,23 +1,22 @@
-//Sign in form validation 
-
 document.addEventListener("DOMContentLoaded", function () {
   const signinFormInput = document.getElementById("signinForm");
   const username1Input = document.getElementById("username1");
   const password1Input = document.getElementById("password1");
   const submit = document.getElementById("submit");
 
-  submit.addEventListener("submit", function (event) {
+  $("#signupForm").submit(function (event) {
     event.preventDefault();
     const username1 = username1Input.value;
     const password1 = password1Input.value;
 
     const userData = JSON.parse(localStorage.getItem("signinForm")) || [];
 
-    const user = signinForm.find((signinForm) => signinForm.username1 === username1 && signinForm.password1 === password1);
+    const user = userData.find(
+      (signinForm) => signinForm.username1 === username1 && signinForm.password1 === password1
+    );
 
     if (user) {
       localStorage.setItem("isSignedIn", "true");
-
       window.location.href = "index.html";
     } else {
       alert("Invalid email or password.");
@@ -25,39 +24,37 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   if (localStorage.getItem("isSignedIn") === "true") {
-
     window.location.href = "index.html";
   }
 });
 
-//sign up form validation 
-
 document.addEventListener("DOMContentLoaded", function () {
-  const signupForm = document.querySelector("signupForm");
+  const signupForm = document.getElementById("signupForm");
+  const submit = signupForm.querySelector("input[type='submit']");
 
-  submit.addEventListener("submit", function (event) {
-      event.preventDefault();
+  $('#signinForm').submit(function (event) {
+    event.preventDefault();
 
-      const usernameInput = document.querySelector("input[name='Username']");
-      const emailInput = document.querySelector("input[name='email']");
-      const passwordInput = document.querySelector("input[name='password']");
-      const password2Input = document.querySelector("input[name='password2']");
+    const usernameInput = document.querySelector("input[name='username']");
+    const emailInput = document.querySelector("input[name='email']");
+    const passwordInput = document.querySelector("input[name='password']");
+    const password2Input = document.querySelector("input[name='password2']");
 
-      const username = usernameInput.value;
-      const email = emailInput.value;
-      const password = passwordInput.value;
-      const password2 = password2Input.value;
+    const username = usernameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const password2 = password2Input.value;
 
-      if (username.trim() === "" || email.trim() === "" || password.trim() === "" || password2.trim() === "") {
-          alert("Please fill in all the required fields");
-          return;
-      }
+    if (username.trim() === "" || email.trim() === "" || password.trim() === "" || password2.trim() === "") {
+      alert("Please fill in all the required fields");
+      return;
+    }
 
-      if (password !== password2) {
-          alert("Passwords are not the same");
-          return;
-      }
+    if (password !== password2) {
+      alert("Passwords are not the same");
+      return;
+    }
 
-      window.location.href = "index.html";
+    window.location.href = "index.html";
   });
 });
